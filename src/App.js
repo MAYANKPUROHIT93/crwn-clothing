27b98daff71fefcf61901d129869235d57aa09c0
MAYANKPUROHIT93 <mayank.prohit@gmail.com>
2020-11-29 01:54:07 +0530

    created project with create react app and added files for home page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import DomeCage from './homepage.component';
+
+function App() {
+  return (
+    <React.StrictMode>
+    <div >
+      <DomeCage />
+    </div>
+    </React.StrictMode>
+  );
+}
+
+export default App;

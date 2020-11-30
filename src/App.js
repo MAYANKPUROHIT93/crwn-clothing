d00a442a32a10413639c275846396b68fb53edd6
MAYANKPUROHIT93 <mayank.prohit@gmail.com>
2020-11-30 17:20:27 +0530

    updating our app with a shop page, collectionpreview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
-import DomeCage from './pages/homepage/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+import {Route} from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
+
+
+
+
 
 function App() {
   return (
     <React.StrictMode>
     <div >
-      <DomeCage />
+      <switch>
+      <Route exact path='/' component={HomePage}/>
+      <Route exact path='/shop' component={ShopPage}/>
+      </switch>
     </div>
     </React.StrictMode>
   );
